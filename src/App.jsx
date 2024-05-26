b0c44bf9ae6d9affa2faf4f6fe840690851caac6
yoannlg <yoann.legouguec.pro@gmail.com>
2024-05-26 16:29:30 +0200

    ♻️ F01: Login Create LoginFom

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,7 @@
-import { useState } from "react"
-import LoginPage from "./components/pages/LoginPage";
+import LoginPage from "./components/pages/login/LoginPage";
 
 function App(){
-    //state
-    const [name, setName] = useState("") 
-
-
-    //comportement
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        setName('')
-
-
-    }
-
-    const handleChange = (e) =>{
-        setName(e.target.value);
-    }
-    const handleClick = ()=>{
-        alert(`Bonjour ${name}`)
-    }
-
-    //render
-    return (
-        <>
-        <LoginPage/>
-            <div>
-                <h1>Bienvenue chez nous !</h1>
-                <p>Connectez-vous</p>
-                <form action="submit" onSubmit={handleSubmit}>
-                    <input onChange={handleChange} value={name} type="text" required name="" id="userName" placeholder="Entrez votre prénom..."/>
-                    <button onClick={handleClick}>Accédeez à votre espace</button>
-                </form>
-                
-            </div>
-        </>
-    )
-
+    return <LoginPage/>
 }
+
 export default App
