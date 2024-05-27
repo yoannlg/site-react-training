5563f06b5ccb208880563948e50d1b4155db5477
yoannlg <yoann.legouguec.pro@gmail.com>
2024-05-27 15:12:15 +0200

    ✨ F02: Create OrderPage and ErrorPage + routing systeme

---
+++ src/components/pages/order/OrderPage.jsx
@@ -0,0 +1,10 @@
+export default function OrderPage() {
+    
+    return (
+        <>
+            <h1> Bonjour nom </h1>
+            <br />
+            <button>Déconnexion</button>
+        </>
+    )
+}
