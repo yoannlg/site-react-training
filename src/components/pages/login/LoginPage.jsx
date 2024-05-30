1860a213d851089cdac0b1d1d59aa8bb28180ada
yoannlg <yoann.legouguec.pro@gmail.com>
2024-05-30 02:47:54 +0200

    💄 F03: Ajout de style et background pour la LoginPage

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -12,10 +12,23 @@ export default function LoginPage() {
 }
 
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
+  ::before {
+    content: "";
+    background: url("/images/burger-background.jpg") rgba(0, 0, 0, 0.5);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+  }
 `;
