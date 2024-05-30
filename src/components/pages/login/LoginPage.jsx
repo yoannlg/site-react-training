3db671153a89733efc9f52da61f5edfbb7cd61be
yoannlg <yoann.legouguec.pro@gmail.com>
2024-05-30 13:01:20 +0200

    💄 F03: LoginPage > Correction bug graphique sur le background

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -17,18 +17,7 @@ const LoginPageStyled = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  ::before {
-    content: "";
-    background: url("/images/burger-background.jpg") rgba(0, 0, 0, 0.5);
-    background-size: cover;
-    background-position: center;
-    background-blend-mode: darken;
-
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: -1;
-  }
+  background: center / cover url("/images/burger-background.jpg"),
+    rgba(0, 0, 0, 0.5);
+  background-blend-mode: darken;
 `;
--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,9 +1,38 @@
 import styled from "styled-components";
 
 export default function Logo() {
-  return <LogoStyled>Logo</LogoStyled>;
+  return (
+    <LogoStyled>
+      <h1>CRAZEE</h1>
+      <img src="/images/logo-orange.png" alt="" />
+      <h1>BURGER</h1>
+    </LogoStyled>
+  );
 }
 
 const LogoStyled = styled.div`
-  background: blue;
+  display: flex;
+  align-items: center;
+  transform: scale(2.5);
+  z-index: 1;
+
+  h1 {
+    display: inline;
+    text-align: center;
+    color: #ffa01b;
+    font-size: 36px;
+    line-height: 1em;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    font-family: "Amatic SC", cursive;
+  }
+
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 60px;
+    width: 80px;
+    margin: 0 5px;
+  }
 `;
