7f03d98c319887606ec4db1633dab0aa6b5ab998
yoannlg <yoann.legouguec.pro@gmail.com>
2024-06-03 14:56:48 +0200

    🔥 F03: OrderPage > Suppression comentaire

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,13 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 
 export default function OrderPage() {
-  //state
-  //const params = useParams() //Sans le destructuring
-  const { userName } = useParams(); //Avec le DESTRUCTURING
-
-  //comportement
-
-  //render
+  const { userName } = useParams();
   return (
     <>
       <h1> Bonjour {userName} </h1>
