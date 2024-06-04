02778b2a8213c916cdc69f8fe988ac5947cc9b06
yoannlg <yoann.legouguec.pro@gmail.com>
2024-06-04 02:26:21 +0200

    🚧 F04: Navbar > mise en place des elements contenaire gauche  et contenaire droit

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -11,11 +11,16 @@ export default function OrderPage() {
           {/* <NavBar /> */}
           <div className="leftSide">LOGO</div>
           <div className="rigthSide">
-            <h1> Bonjour {userName} </h1>
-            <br />
-            <Link to={"/"}>
-              <button>Déconnexion</button>
-            </Link>
+            <div className="containt">
+              <h1>
+                Hey, <span>{userName}</span>
+              </h1>
+              <br />
+              <Link to={"/"}>
+                <a className="logout">Déconnexion</a>
+              </Link>
+            </div>
+            <div className="iconContaint">ICON</div>
           </div>
         </div>
       </div>
@@ -42,7 +47,42 @@ const OrderPageStyled = styled.div`
       align-items: center;
       background: white;
       border-radius: 15px 15px 0 0;
-      box-shadow: 0px 15px 15px gray;
+      box-shadow: 0px 5px 30px gray;
+
+      .rigthSide {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        margin: 27px;
+
+        .containt {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+
+        h1 {
+          color: #747b91;
+          font-size: 16px;
+          font-weight: 400;
+          span {
+            color: orange;
+            font-weight: 700;
+          }
+        }
+        .logout {
+          color: #747b91;
+          font-weight: 400;
+          font-size: 10px;
+        }
+        :link {
+          text-decoration: none;
+        }
+        .iconContaint {
+          margin-right: 70px;
+        }
+      }
     }
   }
 `;
