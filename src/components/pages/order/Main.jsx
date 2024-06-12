c291026e676d64c9d66ac0dc623aa99826c0a33e
yoannlg <yoann.legouguec.pro@gmail.com>
2024-06-12 23:07:48 +0200

    🚧 F05: OrderPage > Mise en place layout des produits

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,8 +1,22 @@
 import styled from "styled-components";
 import { theme } from "../../../theme";
+import Card from "../../reusable-ui/Card";
 
 export default function Main() {
-  return <MainStyled className="main"></MainStyled>;
+  return (
+    <MainStyled className="main">
+      <Card name="UN" className="green" />
+      <Card name="DEUX" className="green" />
+      <Card name="TROIS" className="green" />
+      <Card name="QUATRE" className="green" />
+      <Card name="CINQ" className="green" />
+      <Card name="SIX" className="green" />
+      <Card name="SEPT" className="green" />
+      <Card name="HUIT" className="green" />
+      <Card name="NEUF" className="green" />
+      <Card name="DIX" className="green" />
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.div`
@@ -11,4 +25,17 @@ const MainStyled = styled.div`
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+  display: grid;
+  //grid-template-columns: 350px 350px 350px 350px;
+  //grid-template-columns: repeat(4, 350px);
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px 0px;
+
+  .darkblue {
+    background: darkblue;
+  }
+  .green {
+    background: green;
+  }
 `;
--- src/components/reusable-ui/Card.jsx
+++ src/components/reusable-ui/Card.jsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
+import { theme } from "../../theme";
 
-export default function Card() {
-  return <CardStyled>Card</CardStyled>;
+export default function Card({ name, className }) {
+  return <CardStyled className={className}>{name}</CardStyled>;
 }
 const CardStyled = styled.div`
+  width: 240px;
+  height: 330px;
+  margin-top: 50px;
+  border-radius: ${theme.borderRadius.extraRound};
   box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
 `;
