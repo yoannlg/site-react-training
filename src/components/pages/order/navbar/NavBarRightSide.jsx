f27f297c84bed90ed08732d975a8180bf3fe7c6b
yoannlg <yoann.legouguec.pro@gmail.com>
2024-06-23 14:53:18 +0200

    📦 F06: Add react-toastify package and test

--- src/components/pages/order/navbar/NavBarRightSide.jsx
+++ src/components/pages/order/navbar/NavBarRightSide.jsx
@@ -1,13 +1,39 @@
 import styled from "styled-components";
 import Profile from "./Profile";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function NavBarRightSide({ userName }) {
+  const notify = () =>
+    toast.info("🦄 Wow so easy!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+
   return (
     <NavBarRightSideStyled>
-      {/*       <div className="admin-button">ADMIN BUTTON</div>
-       */}
+      <div className="admin-button">ADMIN BUTTON</div>
+      <button onClick={notify}>toto !</button>
+
       <Profile userName={userName} />
-      <div className="picture"></div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </NavBarRightSideStyled>
   );
 }
