0bfc6e79018f3fb7dfcffc154049cd514eaee4da
yoannlg <yoann.legouguec.pro@gmail.com>
2024-07-23 04:12:59 +0200

    🐛 F07 Admin > Tab > fix bug in CORRECTION files with getTabsConfig function

--- src/components/pages/order/menu/Admin/getTabsConfig.jsx
+++ src/components/pages/order/menu/Admin/getTabsConfig.jsx
@@ -1,7 +1,7 @@
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdModeEditOutline } from "react-icons/md";
 
-export const getTabsConfig = [
+export const getTabsConfig = () => [
   /* {
     index: "chevronUpDown",
     onClick: handleClickOnCollapsedTab,
